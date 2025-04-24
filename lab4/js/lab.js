bd57e913e9ecb47c5433d2f72652e69fd59c26a0
jenniferval20 <jlvallec@ucsc.edu>
2025-04-24 09:06:17 -0700

    idk

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,10 +1,34 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock-Paper-Scissors
+// Jennifer Vallecillo <jlvallec@ucsc.edu>
+// April 21 2025
 
-// Constants
+// Display game options to player (rock, paper, scissors)
+// Get player input
+// Randomly select computer choice
+// Compare player and computer choices
+    // If same, it’s a tie
+    // If player wins, display win message
+    // If computer wins, display lose message
+// Ask if player wants to play again
 
-// Functions
+// Changing a Tire
+// Park the car safely
+    // Find a flat surface
+    // Turn on hazard lights
+    // Apply parking brake
+// Gather necessary tools
+    // Spare tire
+    // Jack
+    // Lug wrench
+// Loosen lug nuts slightly
+// Jack up the car
+// Remove lug nuts and take off the flat tire
+// Mount the spare tire
+// Tighten the lug nuts by hand
+// Lower the car using the jack
+// Fully tighten the lug nuts with the wrench
+// Put away tools and flat tire
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,18 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Jennifer Vallecillo
+// Date: 4/24/25
 
-// Constants
+// Define Variables
+make = "Porshe";
+model = "911 Carerra";
+color = "green";
+year = "2020";
 
-// Functions
+//  calculate
+age = 2025 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+//output
+document.writeln("Make "+ make + "<br>");
+document.writeln("Model "+ model + "<br>");
+document.writeln("Color "+ color + "<br>");
+document.writeln("Year "+ year + "<br>");
