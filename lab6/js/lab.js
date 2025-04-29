bf84e3c7bbfef91b7bf36bd0add3d1d9d2800106
jenniferval20 <jlvallec@ucsc.edu>
2025-04-28 20:53:18 -0700

    yes
    yes

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,21 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Transportation list
+let myTransport = ["Porshe 911 Carerra", "Walking", "Bus", "Rides from friends"];
 
-// Constants
+// Main Ride object
+let myMainRide = {
+  make: "Porshe",
+  model: "911 Carerra",
+  color: "green",
+  year: 2020,
+  age: function () {
+    return 2025 - this.year;
+  }
+};
 
-// Functions
+// Write to #output div
+function outputToPage() {
+  const outputEl = document.getElementById("output");
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
+  outputEl.innerHTML += "Kinds of transportation I use: " + myTransport.join(", ") + "<br><br>";
+  outputEl.innerHTML += "My Main Ride: <pre>" + JSON.stringify(myMainRide, null, 2) + "</pre>";
+  outputEl.innerHTML += "Age of my car in 2025: " + myMainRide.age() + " years old.<br>";
 }
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Call function when page loads
+window.onload = outputToPage;
