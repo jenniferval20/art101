f22472ba90dcf1d0fa97516858ee8c93f9c04eec
jenniferval20 <jlvallec@ucsc.edu>
2025-05-08 09:04:51 -0700

    yes

--- lab 8/js/lab.js
+++ lab 8/js/lab.js
@@ -1,8 +1,30 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-let aColors=["purple","blue","pink","red"];
-function fDoColors  (){
-  $("#output").css("background",color);
+
+// index.js – Anon Functions and Callbacks
+// Author: Jennifer
+// Date: May 8
+
+// Constants
+
+// Functions
+
+// Function to check if a number is even
+function isEven(x) {
+  return (x % 2 == 0);
 }
-fDoColors("purple");
+
+// test function
+console.log("Is 1 even? ", isEven(1));
+console.log("Is 2 even? ", isEven(2));
+
+var array = [100, 81, 4, 16, 42, 144, 10000];
+console.log("My array:", array);
+
+var result = array.map(isEven);
+// should return [true, false, true, true, true, true, true]
+console.log("Test of evenness of array:", result);
+
+var result = array.map(function(x) {
+  return Math.sqrt(x);
+});
+// should return [10, 9, 2, 4, 6.48074069840786, 12, 100]
+console.log("Square root of array:", result);
