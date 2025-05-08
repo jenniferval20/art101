f4f953124d5a4e6a6ad810e54920e2dc813d3258
jenniferval20 <jlvallec@ucsc.edu>
2025-05-08 08:51:35 -0700

    hello
    hello

---
+++ lab 8/js/lab.js
@@ -0,0 +1,8 @@
+// index.js - purpose and description here
+// Author: Your Name
+// Date:
+let aColors=["purple","blue","pink","red"];
+function fDoColors  (){
+  $("#output").css("background",color);
+}
+fDoColors("purple");
