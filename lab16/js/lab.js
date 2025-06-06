baee66fdb222ccec892069d8d7fd749ecbeae672
jenniferval20 <jlvallec@ucsc.edu>
2025-06-05 21:54:10 -0700

    commit

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -1,4 +1,4 @@
-JavaScript
+
 /*
 * Author: Jennifer
 * Created: June 3, 2025
@@ -7,30 +7,30 @@ JavaScript
 
 
 function getComic() {
-   $.ajax({
-       url: "https://api.allorigins.win/get?url=https://xkcd.com/info.0.json",
-       type: "GET",
-       dataType: "json",
-       success: function (response) {
-           // Parse the JSON from the 'contents' string
-           let comicObj = JSON.parse(response.contents);
-           console.log(comicObj); // for debugging
+  $.ajax({
+    url: "https://api.allorigins.win/get?url=https://xkcd.com/info.0.json",
+    type: "GET",
+    dataType: "json",
+    success: function (response) {
+      console.log("Raw response:", response.contents);
 
+      let comicObj = JSON.parse(response.contents);
+      console.log("Parsed comic object:", comicObj);
 
-           let comicHTML = `
-               <h3>${comicObj.title}</h3>
-               <img src="${comicObj.img}" alt="${comicObj.alt}" title="${comicObj.alt}">
-               <p>${comicObj.alt}</p>
-           `;
-           $("#output").html(comicHTML);
-       },
-       error: function (jqXHR, textStatus, errorThrown) {
-           console.log("Error:", textStatus, errorThrown);
-       }
-   });
+      let comicHTML = `
+        <h3>${comicObj.title}</h3>
+        <img src="${comicObj.img}" alt="${comicObj.alt}" title="${comicObj.alt}">
+        <p>${comicObj.alt}</p>
+      `;
+      $("#output").html(comicHTML);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log("Error:", textStatus, errorThrown);
+      $("#output").html("<p>Failed to load comic. Please try again later.</p>");
+    }
+  });
 }
 
-
 $(document).ready(function () {
-   getComic();
+  getComic();
 });
